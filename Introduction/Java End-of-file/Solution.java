1cdee00c3bda0fefd465162e9ee697569291c8e4
Mahbubur Rahman <mahbubur.rahman@saltanatglobal.com>
2020-04-16 19:07:34 +0600

    Introduction all file pushed in one commit

---
+++ Introduction/Java End-of-file/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int i = 1;
+
+        while(input.hasNextLine()){
+            System.out.println(i + " "+input.nextLine());
+            i++;
+        }
+        input.close();
+    }
+}
